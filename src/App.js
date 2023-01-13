06757287294302db53e6673a517733dd9ce961cc
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-01-13 05:42:17 +0100

    props setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import Card from "./components/Card";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import "./style.css"
+import image from "./images/image-12.png"
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
      {/* <Hero/> */}
-     <Card/>
+     <Card
+     img={image}
+     rating="5.0"
+     reviewCount="(6)"
+     country="USA"
+     title="Life Lessons with Katie Zaferes"
+     price="136"
+     />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className='card'>
-      <img src={require("../images/image-12.png")} alt="" className='card-img'/>
+      <img src={props.img}alt="" className='card-img'/>
       <div className="card-stats">
         <img src={require("../images/star.png")} alt="" className='card-star'/>
-        <span>5.0</span>
-        <span className='gray'>(6) &bull; </span>
-        <span className='gray'>USA</span>
+        <span>{props.rating}</span>
+        <span className='gray'>{props.reviewCount} &bull; </span>
+        <span className='gray'>{props.country}</span>
       </div>
-      <p>Life Lessons with Katie Zaferes</p>
-      <p> <span className='bold'>From $136</span> / person</p>
+      <p>{props.title}</p>
+      <p> <span className='bold'>From ${props.price}</span> / person</p>
     </div>
   )
 }
